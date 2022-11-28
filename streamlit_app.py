d70e941c88f731510217174aac505d7071077beb
mubaru <kristof.alfoldi@gmail.com>
2022-11-28 09:50:25 +0100

    Update streamlit_app.py
    adding menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('menu')
+streamlit.text('fish')
+streamlit.text('fish with rice')
+streamlit.text('fish in water')
