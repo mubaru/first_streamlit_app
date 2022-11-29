bbe1d7ab5b46f2c016ebcc18f43ca7fa1980c298
mubaru <kristof.alfoldi@gmail.com>
2022-11-29 16:19:15 +0100

    Update streamlit_app.py
    adding new function to add new fruits to our list, try 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # function to select a fruit to get information about
-def get fruityvice_date(this_fruit_choice):
+def get_fruityvice_date(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
@@ -34,7 +34,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function = fruityvice_date(fruit_choice)
+    back_from_function = get_fruityvice_date(fruit_choice)
     streamlit.dataframe(back_from_function)
     
 except URLError as e:
