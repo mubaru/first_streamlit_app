8722cbec8547aaa3217d219511e1b0ffe283826b
mubaru <kristof.alfoldi@gmail.com>
2022-11-29 11:38:36 +0100

    Update streamlit_app.py
    removing not normalized format line and separating the search result from the url

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-streamlit.text(fruityvice_response.json())
+# normalizing json file
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# adding normalized data to a table
+streamlit.dataframe(fruityvice_normalized)
