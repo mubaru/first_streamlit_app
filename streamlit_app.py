aa39a4191efdc958adbf3ddb7d7512c82c0a0c96
mubaru <kristof.alfoldi@gmail.com>
2022-11-29 14:27:12 +0100

    Update streamlit_app.py
    adding second text bar try 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,4 @@ streamlit.header("The fruit load list:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('The user entered ', add_my_fruit)
-
-streamlit.text('Thanks for adding '+ add_my_fruit)
+streamlit.write('Thanks for adding '+ add_my_fruit)
