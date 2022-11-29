151efa5d7cffd56049eba3925ed14bdf01a98640
mubaru <kristof.alfoldi@gmail.com>
2022-11-29 11:34:22 +0100

    Update streamlit_app.py
    missed method from call

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
